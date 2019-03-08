64a6051ff1fba3c4d0eeaadcf80f3ab2b22da22f
Nan Zheng <chunnan.zheng@uq.net.au>
2019-03-08 11:06:49 +1000

    data prisma schema changes

--- data/src/index.js
+++ data/src/index.js
@@ -1,4 +1,4 @@
-const { prisma } = require('../generated/prisma-client')
+const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 
 const resolvers = {
@@ -59,3 +59,11 @@ const resolvers = {
 }
 
 
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma
+  },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
