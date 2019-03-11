96d803346edd5c549646996a2c6337cc92daa493
Nan Zheng <chunnan.zheng@uq.net.au>
2019-03-11 17:14:46 +1000

    app init edit page/cmp

---
+++ app/src/components/Edit.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import '../styles/template.css'
+
+class Edit extends Component {
+    constructor() {
+        super()
+        this.setState({})
+    }
+
+    render() {
+        return (
+            <div className="Edit">
+                Edit
+            </div>
+        );
+    }
+}
+
+export default Edit;
