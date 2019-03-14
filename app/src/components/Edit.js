d8df43405c896866bf9e5b32143dc3a7e878c099
Nan Zheng <chunnan.zheng@uq.net.au>
2019-03-14 13:25:13 +1000

    app styling Edit modal

--- app/src/components/Edit.js
+++ app/src/components/Edit.js
@@ -3,6 +3,9 @@ import '../styles/App.css';
 import '../styles/template.css'
 import { connect } from 'react-redux';
 import { closeEditModal } from '../actions/closeEditModal'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 
 const mapStateToProps = state => ({
     state
@@ -11,15 +14,23 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     closeEditModal: () => dispatch(closeEditModal())
 })
-
   
 class Edit extends Component {
     render() {
         return (
-            <div className="Edit">
-                { this.props.state.editProductReducer.editModalOpen && 
-                    <button type="button" className="btn btn-sm btn-outline-secondary" onClick={this.props.closeEditModal}>Close</button>
-                 }
+            <div className="Edit modal-open">
+                <Modal show={this.props.state.editProductReducer.editModalOpen} onHide={this.props.closeEditModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        ...
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.props.closeEditModal}>Cancel</Button>
+                        <Button variant="primary">Save Changes</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
