4e498ff2352c54c34d6db4f85aa8f269bb40b0e6
Nan Zheng <chunnan.zheng@uq.net.au>
2019-03-15 15:42:51 +1000

    app modify Edit to cater for updating product 1/2

--- app/src/components/Edit.js
+++ app/src/components/Edit.js
@@ -6,8 +6,7 @@ import { closeEditModal } from '../actions/closeEditModal'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
-// import { ALL_PRODUCTS } from './ProductList'
+import { Query, Mutation } from 'react-apollo'
 
 export const FIND_PRODUCT = gql`
     query Product($id: ID!) {
@@ -21,30 +20,59 @@ export const FIND_PRODUCT = gql`
     }
 `
 
-const EDIT_PRODUCT_DETAILS = ({id}) => (
-    <Query query={FIND_PRODUCT} variables={{id}}>
-        {({ loading, error, data }) => {
-            if (loading) return null
-            if (error) return <div>{error.message}</div>
-            const product = data.product
-            return (
-                <div>
-                    <label for="editProductId" className="m-r-10 m-t-10 display-block">ID</label>
-                    <input type="text" name="editProductId" disabled placeholder={product.id} className="w-100pc"/>
-                    <br />
-                    <label for="editProductName" className="m-r-10 m-t-10 display-block">Product name</label>
-                    <input type="text" name="editProductName" value={product.name} className="w-100pc"/>
-                    <br />
-                    <label for="editProductPhoto" className="m-r-10 m-t-10 display-block">Image path</label>
-                    <input type="text" name="editProductPhoto" value={product.photo} className="w-100pc"/>
-                    <br />
-                    <label for="editProductDesc" className="m-r-10 m-t-10 display-block">Product description</label>
-                    <textarea name="editProductDesc" value={product.desc} className="w-100pc" rows="10"></textarea>
-                </div>
-            )
-        }}
-    </Query>
-)
+export const UPDATE_PRODUCT = gql`
+    mutation updateProduct($id: ID!, $input: ProductUpdateInput) {
+        updateProduct(id: $id, input: $input) {
+            id
+            name
+            price
+            desc
+            photo
+        }
+    }
+`
+
+export const ADD_PRODUCT = gql`
+    mutation createProduct($name: String!) {
+        createProduct(name: $name) {
+            id
+            name
+            price
+            desc
+            photo
+        }
+    }
+`
+
+// const GET_PRODUCT_DETAILS = ({id}) => (
+//     <Query query={FIND_PRODUCT} variables={{id}}>
+//         {({ loading, error, data }) => {
+//             if (loading) return null
+//             if (error) return <div>{error.message}</div>
+//             const product = data.product
+//             return (
+//                 <div>
+//                     <label htmlFor="editProductId" className="m-r-10 m-t-10 display-block">ID</label>
+//                     <input type="text" name="editProductId" disabled placeholder={product.id} className="w-100pc"/>
+//                     <br />
+//                     <label htmlFor="editProductName" className="m-r-10 m-t-10 display-block">Product name</label>
+//                     <input type="text" name="editProductName" defaultValue={product.name} className="w-100pc"/>
+//                     <br />
+//                     <label htmlFor="editProductPrice" className="m-r-10 m-t-10 display-block">Price</label>
+//                     <input type="text" name="editProductPrice" defaultValue={product.price} className="w-100pc"/>
+//                     <br />
+//                     <label htmlFor="editProductPhoto" className="m-r-10 m-t-10 display-block">Image path</label>
+//                     <input type="text" name="editProductPhoto" defaultValue={product.photo} className="w-100pc"/>
+//                     <br />
+//                     <label htmlFor="editProductDesc" className="m-r-10 m-t-10 display-block">Product description</label>
+//                     <textarea name="editProductDesc" defaultValue={product.desc} className="w-100pc" rows="10"></textarea>
+//                 </div>
+//             )
+//         }}
+//     </Query>
+// )
+
+
 
 const mapStateToProps = state => ({
     state
@@ -59,6 +87,63 @@ class Edit extends Component {
         let id = this.props.state.editProductReducer.productId
         return {id}
     }
+
+    GET_PRODUCT_DETAILS = ({id}) => (
+        <Query query={FIND_PRODUCT} variables={{id}}>
+            {({ loading, error, data }) => {
+                if (loading) return null
+                if (error) return <div>{error.message}</div>
+                const product = data.product
+                return (
+                    <div>
+                        <label htmlFor="editProductId" className="m-r-10 m-t-10 display-block">ID</label>
+                        <input type="text" name="editProductId" disabled defaultValue={product.id} className="w-100pc"/>
+                        <br />
+                        <label htmlFor="editProductName" className="m-r-10 m-t-10 display-block">Product name</label>
+                        <input type="text" name="editProductName" defaultValue={product.name} className="w-100pc"/>
+                        <br />
+                        <label htmlFor="editProductPrice" className="m-r-10 m-t-10 display-block">Price</label>
+                        <input type="text" name="editProductPrice" defaultValue={product.price} className="w-100pc"/>
+                        <br />
+                        <label htmlFor="editProductPhoto" className="m-r-10 m-t-10 display-block">Image path</label>
+                        <input type="text" name="editProductPhoto" defaultValue={product.photo} className="w-100pc"/>
+                        <br />
+                        <label htmlFor="editProductDesc" className="m-r-10 m-t-10 display-block">Product description</label>
+                        <textarea name="editProductDesc" defaultValue={product.desc} className="w-100pc" rows="10"></textarea>
+                    </div>
+                )
+            }}
+        </Query>
+    )
+
+    UPDATE_PRODUCT_DETAILS = () => {
+        console.log("111111")
+
+           let id = document.getElementsByName('editProductId')[0].value
+           let name = document.getElementsByName('editProductName')[0].value
+           let price = document.getElementsByName('editProductPrice')[0].value
+           let photo = document.getElementsByName('editProductPhoto')[0].value
+           let desc = document.getElementsByName('editProductDesc')[0].value
+           let input = {
+               name: name,
+               price: price,
+               photo: photo,
+               desc: desc
+           }
+           let args = {
+               id: id,
+               input: input
+           }
+
+       console.log("2222222")
+
+       return (
+            <Mutation mutation={UPDATE_PRODUCT} variables={args}>
+                {<div>TODO: change</div> }
+            </Mutation>
+       )
+    }
+
     render() {
         return (
             <div className="Edit">
@@ -68,11 +153,12 @@ class Edit extends Component {
                         <Modal.Title>Edit product details</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        {EDIT_PRODUCT_DETAILS(this._getQueryVariables())}
+                        {this.GET_PRODUCT_DETAILS(this._getQueryVariables())}
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={this.props.closeEditModal}>Cancel</Button>
-                        <Button variant="primary">Save Changes</Button>
+                        {/* {this.UPDATE_PRODUCT_DETAILS()} */}
+                        <Button variant="primary" onClick={this.UPDATE_PRODUCT_DETAILS}>Save Changes</Button>
                     </Modal.Footer>
                 </Modal>
 
