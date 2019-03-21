918a2e796a2ccf9d4670d745e87735dcd4efc161
Nan Zheng <chunnan.zheng@uq.net.au>
2019-03-21 10:27:27 +1000

    data revert installation, because of wrong locati

--- data/src/index.js
+++ data/src/index.js
@@ -1,7 +1,3 @@
-import { split } from 'apollo-link'
-import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
-
 const { prisma } = require('../generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 const Query = require('../resolvers/Query')
