a8b7aa91bd82de18deb36f7247ed2138414cb12e
Nan Zheng <chunnan.zheng@uq.net.au>
2019-03-26 11:54:51 +1000

    app display shopping cart

--- app/src/components/ShoppingCart.js
+++ app/src/components/ShoppingCart.js
@@ -7,6 +7,11 @@ import { closeShoppingCart } from '../actions/closeShoppingCart'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
+import { FIND_PRODUCT } from './Edit'
+
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
 const mapStateToProps = state => ({
     state
 })
@@ -16,24 +21,48 @@ const mapDispatchToProps = dispatch => ({
 })
 
 class ShoppingCart extends Component {
-  render() {
-    return (
-        <Modal show={this.props.state.shoppingCartReducer.shoppingCartOpen} 
-            onHide={this.props.closeShoppingCart}>
-            <Modal.Header closeButton>
-                <Modal.Title>View shopping cart</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="light" onClick={this.props.closeShoppingCart}>Proceed shopping</Button>
-                <Button variant="warning" onClick={this.props.closeShoppingCart} className="text-white">Checkout</Button>
-            </Modal.Footer>
-        </Modal>
+    getProduct = (id) => (
+        <Query query={FIND_PRODUCT} variables={{id}}>
+            {({ loading, error, data }) => {
+                if (loading) return null
+                if (error) return <div>{error.message}</div>
+                const product = data.product
+                return (
+                    <li>
+                        {product.name} - ${product.price}.00
+                    </li>
+                )
+            }}
+        </Query>
+    )
 
-    );
-  }
+    render() {
+        return (
+            <Modal show={this.props.state.shoppingCartReducer.shoppingCartOpen} 
+                onHide={this.props.closeShoppingCart}>
+                <Modal.Header closeButton>
+                    <Modal.Title>View shopping cart</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    { this.props.state.shoppingCartReducer.products !== '' &&
+                        this.props.state.shoppingCartReducer.products.split(',').map(productId => {
+                            return <ul>
+                                {this.getProduct(productId)}
+                            </ul>
+                        })
+                    }
+                    { this.props.state.shoppingCartReducer.products === '' &&
+                        <div>Products added to your shopping cart will be displayed here.</div>
+                    }
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="light" onClick={this.props.closeShoppingCart}>Proceed shopping</Button>
+                    <Button variant="warning" onClick={this.props.closeShoppingCart} className="text-white">Checkout</Button>
+                </Modal.Footer>
+            </Modal>
+
+        );
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart)
